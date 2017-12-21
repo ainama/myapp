078d4ff739fc659dad834d377455a3475897e9b3
[zhangning] <[zhangning@frogshealth.com]>
2017-12-21 11:37:13 +0800

    数据库迁移

--- routes/community/api.js
+++ routes/community/api.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-var query = require('../../tools/mysql_server.js');
+var query = require('../../tools/community_server.js');
 
 /**
  * test
  * @method /api/community/test
  */
 router.get('/test', function (req, res) {
-  var sql = 'SELECT * FROM test WHERE status=1 ORDER BY update_time DESC';
+  var sql = 'select * from t_user;';
   query(sql, null, function (error, results, fields) {
     if (error) throw error;
     res.send({ code: 10000, msg: results });

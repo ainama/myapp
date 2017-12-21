12b80662807f64ab93f53304207d41fd7b51674e
[zhangning] <[zhangning@frogshealth.com]>
2017-12-21 13:37:32 +0800

    config demo

---
+++ config/demo_config.js
@@ -0,0 +1,9 @@
+var mysql_config = {
+  host     : '****.***.com',
+  user     : '********',
+  password : '********',
+  database : '********_db',
+  port: '3306'
+};
+
+module.exports = mysql_config;
